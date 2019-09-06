8a5216ece48f4df2da34f730015114edf8fd6b1e
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-06 11:50:04 -0400

    centered loading h2

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <h1>Women's World Cup Player Search Interest June-July 2019</h1>
       <div className="card-grid">
-        {!playerData ? <h2>Loading</h2> : playerData.map(player => <PlayerCard player={player} />)}
+        {!playerData ? <h2 className="loading">Loading</h2> : playerData.map(player => <PlayerCard player={player} />)}
       </div>
     </div>
   );
