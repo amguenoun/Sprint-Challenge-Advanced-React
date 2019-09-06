c399ac70db035304b7f21236cd3a28b61e951f82
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-06 11:31:32 -0400

    useAxios custom hook works and data

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import './App.css';
 
+import useAxios from './components/hooks/useAxios';
+
 function App() {
+  const [playerData, setUrl] = useAxios(`http://localhost:5000/api/players`);
+
   return (
     <div className="App">
       Hey
