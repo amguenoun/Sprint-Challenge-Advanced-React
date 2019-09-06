7cc1538d4a0bfe9c04ea3a0514e76ae84ce552b6
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-06 11:42:12 -0400

    player data now displays on page

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,17 @@ import 'semantic-ui-css/semantic.min.css'
 import './App.css';
 
 import useAxios from './components/hooks/useAxios';
+import PlayerCard from './components/PlayerCard';
 
 function App() {
   const [playerData, setUrl] = useAxios(`http://localhost:5000/api/players`);
 
   return (
     <div className="App">
-      Hey
+      <h1>Women's World Cup Player Search Interest June-July 2019</h1>
+      <div className="card-grid">
+        {!playerData ? <h2>Loading</h2> : playerData.map(player => <PlayerCard player={player} />)}
+      </div>
     </div>
   );
 }
