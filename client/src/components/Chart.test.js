01acd36c2716ca764b098275bd535d012928f425
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-06 13:34:51 -0400

    added semicolon

--- client/src/components/Chart.test.js
+++ client/src/components/Chart.test.js
@@ -3,6 +3,6 @@ import { render } from '@testing-library/react';
 import Chart from './Chart';
 
 test('Chart renders without crashing', () => {
-    const data = [["hey", 0]]
+    const data = [["hey", 0]];
     render(<Chart barInput={data} />);
 })
