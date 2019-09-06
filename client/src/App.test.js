dc6f0e336fa928cc985a6b490dea5971e2b8df97
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-06 12:19:41 -0400

    modified app test js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -5,3 +5,8 @@ import App from './App';
 test('App renders without crashing', () => {
   render(<App />);
 })
+
+test('App contains a header', () => {
+  const { getByText } = render(<App />);
+  getByText(/world cup player/i)
+})
