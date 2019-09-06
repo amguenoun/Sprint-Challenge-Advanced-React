a1cbec83ece109c2b9602c5957e694cedc03f01d
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-06 13:26:55 -0400

    added axis names to chart

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -5,7 +5,7 @@ import 'chart.js'
 const Chart = (props) => {
     return (
         <div className="chart">
-            <BarChart data={props.barInput} height='1500px' />
+            <BarChart data={props.barInput} height='1500px' ytitle="Name" xtitle='Searches' />
         </div>
     )
 }
