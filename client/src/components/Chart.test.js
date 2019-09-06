5b5e96406ae2a220236941ecc5e1bca7e567132c
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-06 13:29:58 -0400

    created a test for chart.js

---
+++ client/src/components/Chart.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Chart from './Chart';
+
+test('Chart renders without crashing', () => {
+    const data = [["hey", 0]]
+    render(<Chart barInput={data} />);
+})
